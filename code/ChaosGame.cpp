08f604f11e893a598e694dba3f864042652b93bf
Your Name <you@example.com>
2024-11-16 19:46:01 -0800

    working#1

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cstdlib>
 
 //Make the code easier to type with "using namespace"
 using namespace sf;
@@ -11,13 +12,26 @@ using namespace std;
 
 int main()
 {
+	srand(static_cast<unsigned>(time(0)));
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	VideoMode vm(500, 500);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!!", Style::Default);
 	
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
+
+	Font font;
+	font.loadFromFile("Arial.ttf");
+	if (!font.loadFromFile("Arial.ttf"))
+	{
+		cout << "file not found" << endl;
+	}
+	
+	Text text;
+	text.setCharacterSize(24);
+	text.setFillColor(Color::Yellow);
+	text.setFont(font);
 
 	while (window.isOpen())
 	{
@@ -48,6 +62,8 @@ int main()
 			    }
 			    else if(points.size() == 0)
 			    {
+				points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+				cout << "4th click pushed to vector!" << endl;
 				///fourth click
 				///push back to points vector
 			    }
@@ -64,26 +80,50 @@ int main()
 		****************************************
 		*/
 	
-		if(points.size() > 0)
+		if (points.size() > 0)
 		{
-		    ///generate more point(s)
-		    ///select random vertex
-		    ///calculate midpoint between random vertex and the last point in the vector
-		    ///push back the newly generated coord.
+			for (int i = 0; i < 100; i++)
+			{
+				///generate more point(s)
+				///select random vertex
+				int randV = rand() % 3;
+				///calculate midpoint between random vertex and the last point in the vector
+				float midPointX = (points.back().x + vertices[randV].x) / 2;
+				float midPointY = (points.back().y + vertices[randV].y) / 2;
+				///push back the newly generated coord.
+				points.push_back(Vector2f(midPointX, midPointY));
+			}
 		}
-	
+
 		/*
 		****************************************
 		Draw
 		****************************************
 		*/
 		window.clear();
+		if (vertices.size() < 3)
+		{
+			text.setString("Choose 3 points for your triangle");
+			window.draw(text);
+		}
+		else {
+			text.setString("Choose 4th point to start");
+			window.draw(text);
+		}
+		
 		for(int i = 0; i < vertices.size(); i++)
 		{
 		    RectangleShape rect(Vector2f(10,10));
 		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
 		    rect.setFillColor(Color::Blue);
 		    window.draw(rect);
+		}
+		for (int i = 0; i < points.size(); i++)
+		{
+			CircleShape circ(1);
+			circ.setPosition(Vector2f(points[i].x, points[i].y));
+			circ.setFillColor(Color::White);
+			window.draw(circ);
 		}
 		window.display();
 	}

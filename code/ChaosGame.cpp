023cbfde644cdd9b166fafddd062cf820c1f84bc
root <root@DESKTOP-KDB5M5R.>
2024-11-19 23:47:01 -0800

    c/cc rotation

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -1,15 +1,14 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <cstdlib>
 #include <cmath>
+#include <cstdlib>
 
 using namespace sf;
 using namespace std;
 
-Vector2f rotatePoint(const Vector2f& point, const Vector2f& pivot, float angle) {
+Vector2f rotatePoint(Vector2f& point, Vector2f& pivot, float angle)
+{
     float rad = angle * (M_PI / 180.0);
     float cosA = cos(rad);
     float sinA = sin(rad);
@@ -20,19 +19,19 @@ Vector2f rotatePoint(const Vector2f& point, const Vector2f& pivot, float angle) 
     return Vector2f(xNew, yNew);
 }
 
-
-float distance(const Vector2f& p1, const Vector2f& p2) {
+float distance(const Vector2f& p1, const Vector2f& p2)
+{
     return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 }
 
-
-Color getColorBasedOnDistance(const Vector2f& point, const Vector2f& centroid) {
+Color getColor(const Vector2f& point, const Vector2f& centroid)
+{
     float dist = distance(point, centroid);
-    float maxDistance = 100.0f;
+    float maxDistance = 90;
     float ratio = dist / maxDistance;
 
     int r = 255 * ratio;
-    int g = (sin(ratio * M_PI) + 1) * 127.5f;
+    int g = (sin(ratio * M_PI) + 1) * 100;
     int b = 255 * (1 - ratio);
 
     return Color(r, g, b);
@@ -40,29 +39,33 @@ Color getColorBasedOnDistance(const Vector2f& point, const Vector2f& centroid) {
 
 int main()
 {
-    srand(static_cast<unsigned>(time(0)));
-
-    VideoMode vm(1700, 800);
+    VideoMode vm(700, 700);
     RenderWindow window(vm, "Chaos Game!!", Style::Default);
+    window.setFramerateLimit(60);
 
     vector<Vector2f> vertices;
     vector<Vector2f> points;
-
-    Font font;
-    font.loadFromFile("Arial.ttf");
-    if (!font.loadFromFile("Arial.ttf"))
-    {
-        cout << "file not found" << endl;
-    }
-
-    Text text;
-    text.setCharacterSize(24);
-    text.setFillColor(Color::Yellow);
-    text.setFont(font);
-
-    float angle = 1.0;
+    float angle = 0.7;
     bool rotate = false;
     int spawnSpeed = 2;
+
+    Font font;
+    if (!font.loadFromFile("Arial.ttf"))
+    {
+        cerr << "Error: Font file not found!" << endl;
+        return -1;
+    }
+
+    Text text, speedText;
+    text.setFont(font);
+    text.setCharacterSize(24);
+    text.setFillColor(Color::Yellow);
+    text.setPosition(10, 40);
+
+    speedText.setFont(font);
+    speedText.setCharacterSize(24);
+    speedText.setFillColor(Color::White);
+    speedText.setPosition(10, 10);
 
     while (window.isOpen())
     {
@@ -73,30 +76,31 @@ int main()
             {
                 window.close();
             }
-	    if (event.type == sf::Event::MouseWheelMoved)
-	    {
-		if (spawnSpeed + event.mouseWheel.delta >= 0) { spawnSpeed += event.mouseWheel.delta; }
-		else { spawnSpeed = 0; }
-		cout << "Current spawn rate: " << spawnSpeed << endl;
-	    }
+
+            if (event.type == sf::Event::MouseWheelMoved)
+            {
+                spawnSpeed += event.mouseWheel.delta;
+                spawnSpeed = max(-static_cast<int>(points.size()), spawnSpeed);
+                cout << "Current spawn rate: " << spawnSpeed << endl;
+            }
+
             if (event.type == Event::MouseButtonPressed)
             {
                 if (event.mouseButton.button == Mouse::Right)
                 {
                     rotate = !rotate;
-		    if (rotate) { cout << "Rotation started" << endl; }
-		    else { cout << "Rotation ended" << endl; }
+                    if (rotate) { angle = -angle; }
                 }
 
                 if (event.mouseButton.button == Mouse::Left)
                 {
-                    cout << "Left mouse " << "x: " << event.mouseButton.x << "y: " << event.mouseButton.y << endl;
+                    cout << "Left x: " << event.mouseButton.x << ", y: " << event.mouseButton.y << endl;
 
                     if (vertices.size() < 3)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
-                    else if (points.size() == 0)
+                    else if (points.empty())
                     {
                         points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
@@ -109,64 +113,73 @@ int main()
             window.close();
         }
 
-        if (vertices.size() == 3 and rotate) {
-            float centerX = (vertices[0].x + vertices[1].x + vertices[2].x) / 3;
-            float centerY = (vertices[0].y + vertices[1].y + vertices[2].y) / 3;
-            Vector2f centroid(centerX, centerY);
-            for (int i = 0; i < 3; i++) {
-                vertices[i] = rotatePoint(vertices[i], centroid, angle);
+        if (vertices.size() == 3 && rotate)
+        {
+            Vector2f centroid((vertices[0].x + vertices[1].x + vertices[2].x) / 3, (vertices[0].y + vertices[1].y + vertices[2].y) / 3);
+
+            for (Vector2f& vertex : vertices)
+            {
+                vertex = rotatePoint(vertex, centroid, angle);
             }
 
-            for (unsigned long int i = 0; i < points.size(); i++) {
-                points[i] = rotatePoint(points[i], centroid, angle);
+            for (Vector2f& point : points)
+            {
+                point = rotatePoint(point, centroid, angle);
             }
         }
 
-        if (points.size() > 0 && spawnSpeed != 0)
+        if (!points.empty() && spawnSpeed > 0)
         {
-            for (int i = 0; i <= spawnSpeed; i++)
+            for (int i = 0; i < spawnSpeed; ++i)
             {
                 int randV = rand() % 3;
-                float midPointX = (points.back().x + vertices[randV].x) / 2;
-                float midPointY = (points.back().y + vertices[randV].y) / 2;
-                points.push_back(Vector2f(midPointX, midPointY));
+                Vector2f midPoint((points.back().x + vertices[randV].x) / 2, (points.back().y + vertices[randV].y) / 2);
+                points.push_back(midPoint);
+            }
+        }
+        else if (spawnSpeed < 0)
+        {
+            for (int i = 0; i < -spawnSpeed; ++i)
+            {
+                if (!points.empty()) { points.pop_back(); }
+                else { break; }
             }
         }
 
-        if (angle >= 360.0) angle = 0.0;
+        if (vertices.size() < 3) { text.setString("Choose 3 points for your triangle"); }
+        else if (points.empty()) { text.setString("Choose a 4th point to start"); }
+        else { text.setString(""); }
+
+        speedText.setString("Speed: " + to_string(spawnSpeed));
 
         window.clear();
-        if (vertices.size() < 3)
-        {
-            text.setString("Choose 3 points for your triangle");
-            window.draw(text);
-        }
-        else if (points.size() == 0)
-        {
-            text.setString("Choose 4th point to start");
-            window.draw(text);
-        }
 
-        for (long unsigned int i = 0; i < vertices.size(); i++)
+        for (const Vector2f& vertex : vertices)
         {
             RectangleShape rect(Vector2f(10, 10));
-            rect.setPosition(vertices[i]);
+            rect.setPosition(vertex);
             rect.setFillColor(Color::Blue);
             window.draw(rect);
         }
 
-        if (vertices.size() == 3) {
-            float centerX = (vertices[0].x + vertices[1].x + vertices[2].x) / 3;
-            float centerY = (vertices[0].y + vertices[1].y + vertices[2].y) / 3;
-            Vector2f centroid(centerX, centerY);
-            for (long unsigned int i = 0; i < points.size(); i++)
+        if (vertices.size() == 3)
+        {
+            Vector2f centroid(
+                (vertices[0].x + vertices[1].x + vertices[2].x) / 3,
+                (vertices[0].y + vertices[1].y + vertices[2].y) / 3
+            );
+
+            for (const Vector2f& point : points)
             {
                 CircleShape circ(1);
-                circ.setPosition(points[i]);
-                circ.setFillColor(getColorBasedOnDistance(points[i], centroid));
+                circ.setPosition(point);
+                circ.setFillColor(getColor(point, centroid));
                 window.draw(circ);
             }
         }
+
+        window.draw(text);
+        window.draw(speedText);
 
         window.display();
     }
